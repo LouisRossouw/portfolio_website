7d9ae7a62c74286e783167cd92b378ad2a55c8a7
Louis Rossouw <80905013+LouisRossouw@users.noreply.github.com>
2022-12-26 22:20:54 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$b^*4g7&8=*9o(lb!*rr7qa-gj(^*1d+nj6panbm5(s$rt6r$7'
+SECRET_KEY = os.getenv("PORTFOLIO_WEBSITE_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
