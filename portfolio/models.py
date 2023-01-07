91732dd352b9e92483e270e061514d984c12eb63
unknown <lourou99@gmail.com>
2023-01-07 20:58:03 +0200

    extra attrs

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Portfolio(models.Model):
     
     title = models.CharField(max_length=100)
+    project_acronym = models.CharField(max_length=3)
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FilePathField(path="/portfolio")
