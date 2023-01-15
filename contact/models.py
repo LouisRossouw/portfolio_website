6d428ff034dfc0f3c2d31647813098797120642d
unknown <lourou99@gmail.com>
2023-01-15 21:51:33 +0200

    ignore_list / email_list objs

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class ignore_list(models.Model):
+    email = models.TextField()
+
+
+
+class Email_list(models.Model):
+    email = models.TextField(default=None, blank=True)
+    name = models.TextField(default=None, blank=True)
+    message = models.TextField(default=None, blank=True)
+    subject = models.TextField(default=None, blank=True)
+    time = models.TimeField(auto_now=True)
+
+    
+
+
+
+
+if __name__ == "__main__":
+
+    print(models)
