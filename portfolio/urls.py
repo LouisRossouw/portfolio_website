30c8348f8ed8408da1dc8d02e1599cc157da0e30
unknown <lourou99@gmail.com>
2023-01-19 22:25:13 +0200

    added Media urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.portfolio_index, name="portfolio_index"),
     path("<int:pk>/", views.portfolio_detail, name="portfolio_detail"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
