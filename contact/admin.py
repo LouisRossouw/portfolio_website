d525fdd839ef0ff74a82ca77cd46a71911f91ef9
unknown <lourou99@gmail.com>
2023-01-19 22:32:52 +0200

    register

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ignore_list, Email_list
 
-# Register your models here.
+# # Register your models here.
+# admin.site.register([ignore_list, Email_list])
+
+
+class Ignore_list_Admin(admin.ModelAdmin):
+    list_display = ("email",)
+
+
+class Email_list_Admin(admin.ModelAdmin):
+    list_display = ("name", "email", "time",)
+
+admin.site.register(ignore_list, Ignore_list_Admin)
+admin.site.register(Email_list, Email_list_Admin)
