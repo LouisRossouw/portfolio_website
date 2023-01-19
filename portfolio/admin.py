852b120744e6455aaf465f9af86c5f2400ebee88
unknown <lourou99@gmail.com>
2023-01-19 22:26:58 +0200

    register

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Projects
 
-# Register your models here.
+# # Register your models here.
+
+class Portfolio_admin(admin.ModelAdmin):
+    list_display = ("title", "project_acronym", "description", "role", "technology", "image_main", "myshots_image", "raw_video", "clips_video", "blog", "produced_at", "youtube_link", "youtube_embed", "tag", "tags")
+
+
+
+admin.site.register(Projects, Portfolio_admin)
+
