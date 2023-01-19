945912a94de846bebead566e015fa7e4175272aa
unknown <lourou99@gmail.com>
2023-01-19 22:25:48 +0200

    callbacks for img uploads for portfolio admin updates.

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,19 +1,42 @@
 from django.db import models
 
-class Portfolio(models.Model):
+def main_image_path(instance, filename):
+    path = f"portfolio/{instance.project_acronym}.png"
+    return path
+
+def myshots_image_path(instance, filename):
+    path = "portfolio/additional_images/" + f"{instance.project_acronym}/myshots/{filename}"
+    return path
+
+def raw_video_path(instance, filename):
+    path = "portfolio/additional_images/" + f"{instance.project_acronym}/raw/{filename}"
+    return path
+
+def clips_video_path(instance, filename):
+    path = "portfolio/additional_images/" + f"{instance.project_acronym}/clips/{filename}"
+    return path
+
+
+
+class Projects(models.Model):
     
     title = models.CharField(max_length=100, default=None)
     project_acronym = models.CharField(max_length=3, default=None)
     description = models.TextField(default=None)
-    additional_description = models.TextField(default=None)
+    role = models.TextField(default=None)
     technology = models.CharField(max_length=20, default=None)
-    image = models.FilePathField(path="/portfolio")
 
-    
+    image_main = models.ImageField(upload_to=main_image_path, blank=True, null=True)
+
+    myshots_image = models.ImageField(upload_to=myshots_image_path, blank=True, null=True)
+    raw_video = models.FileField(upload_to=raw_video_path, blank=True, null=True)
+    clips_video = models.FileField(upload_to=clips_video_path, blank=True, null=True)
+
     blog = models.TextField(default=None)               # link to my blog webpage for specific project.
     produced_at = models.TextField(default=None)        # link to company.
     youtube_link = models.TextField(default=None)       # link to a youtube video.
     youtube_embed = models.TextField(default=None)      # link to an embeded video.
     tag = models.TextField(default=None)                # add a tag / film / advertising / etc.
+    tags = models.TextField(default=None)
 
 
