c71f1fe4a6ca9ac3177b2aad7c7cbed6ac9c5363
unknown <lourou99@gmail.com>
2023-01-22 22:40:48 +0200

    removed google recapture from form

--- contact/forms.py
+++ contact/forms.py
@@ -1,11 +1,13 @@
 from django import forms
-from captcha.fields import ReCaptchaField
+from django.core.exceptions import ValidationError
+# from captcha.fields import ReCaptchaField
 
 class ContactForm(forms.Form):
-    
+
     yourname = forms.CharField(max_length=100, label='Your Name')
-    email = forms.EmailField(required=False, label='Your e-mail address')
+    postbox = forms.EmailField(required=True, label='Your e-mail address')
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
 
-    captcha = ReCaptchaField()
+
+
