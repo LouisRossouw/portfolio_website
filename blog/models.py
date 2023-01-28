a9d07d961f678185b7989c131fbcabb3cdf12868
unknown <lourou99@gmail.com>
2023-01-28 12:41:07 +0200

    basic blog with wagtail

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,47 @@
 from django.db import models
 
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from wagtail.search import index
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=20)
+def main_image_path(instance, filename):
+    """ creates a path for cover images in media/blog/blog name/cover/ """
+
+    blog_name = str(instance).replace(" ", "_")
+    path = f"blog/{blog_name}/cover/{filename}"
+
+    return path
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
-    created_on = models.DateField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
-    categories  = models.ManyToManyField('Category', related_name='posts')
+
+class BlogIndexPage(Page):
+    """ Attributes for the main blog page that lists all the blogs. """
+
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
 
 
-class Comment(models.Model):
-    author = models.CharField(max_length=60)
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+class BlogPage(Page):
+    """ Attributes for the individual blog. """
+
+    cover_img = models.ImageField(upload_to=main_image_path, blank=True, null=True)
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('cover_img'),
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+    ]
