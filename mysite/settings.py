6e92b2ff3851e72372269f52be5b5e07911563d6
Louis <lourou99@gmail.com>
2026-04-19 16:25:02 +0200

    Docker changes to host on coolify

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,13 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("PORTFOLIO_WEBSITE_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEV = True if os.getenv('APP_ENV') == "dev" else False
+DEBUG = DEV
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(",")
+
 
 # Application definition
 
